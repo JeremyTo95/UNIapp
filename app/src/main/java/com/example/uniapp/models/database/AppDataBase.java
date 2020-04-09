055e8f849f354540268dc7e8b626014e03481d32
JeremyTo95 <jeremy.tourari@gmail.com>
2020-04-09 03:08:13 +0200

    include AppDataBase - bis

---
+++ app/src/main/java/com/example/uniapp/models/database/AppDataBase.java
@@ -0,0 +1,95 @@
+package com.example.uniapp.models.database;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.uniapp.controllers.activities.MainActivity;
+import com.example.uniapp.models.database.dao.pointFFN.PointFFN;
+import com.example.uniapp.models.database.dao.pointFFN.PointFFNAPI;
+import com.example.uniapp.models.database.dao.pointFFN.PointFFNDAO;
+import com.example.uniapp.models.database.dao.race.Race;
+import com.example.uniapp.models.database.dao.race.RaceDAO;
+import com.example.uniapp.models.database.dao.training.Training;
+import com.example.uniapp.models.database.dao.training.TrainingDAO;
+import com.example.uniapp.models.database.dao.trainingblock.TrainingBlock;
+import com.example.uniapp.models.database.dao.user.User;
+import com.example.uniapp.models.database.dao.user.UserDAO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@TypeConverters({Converters.class})
+@Database(entities = {User.class, PointFFN.class, Race.class, Training.class}, version = 1, exportSchema = true)
+public abstract class AppDataBase extends RoomDatabase {
+
+    private static volatile AppDataBase INSTANCE;
+    private static final int NUMBER_OF_THREAD = 20;
+    public static final ExecutorService dataWriterExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREAD);
+    public static String URL_DATA = "https://raw.githubusercontent.com/JeremyTo95/myData/";
+
+    public abstract UserDAO     userDAO();
+    public abstract PointFFNDAO pointFFNDAO();
+    public abstract RaceDAO     raceDAO();
+    public abstract TrainingDAO trainingDAO();
+
+    public static AppDataBase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDataBase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDataBase.class, "uni_app_db").addCallback(roomDatabaseCallback).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback roomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            dataWriterExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    PointFFNDAO pointFFNDAO = INSTANCE.pointFFNDAO();
+                    pointFFNDAO.deleteAllPointFFN();
+
+
+                    Gson gson = new GsonBuilder().setLenient().create();
+                    Retrofit retrofit = new Retrofit.Builder().baseUrl(AppDataBase.URL_DATA).addConverterFactory(GsonConverterFactory.create(gson)).build();
+                    PointFFNAPI pointFFNAPI = retrofit.create(PointFFNAPI.class);
+                    Call<List<PointFFN>> call = pointFFNAPI.getResponsePointsFFN();
+
+                    call.enqueue(new retrofit2.Callback<List<PointFFN>>() {
+                        @Override
+                        public void onResponse(Call<List<PointFFN>> call, Response<List<PointFFN>> response) {
+                            if (response.isSuccessful() && response.body() != null) {
+                                List<PointFFN> pointFFNList = response.body();
+                                for (int i = 0; i < pointFFNList.size(); i++) {
+                                    pointFFNDAO.insertPointFFN(pointFFNList.get(i));
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<PointFFN>> call, Throwable t) { }
+                    });
+                }
+            });
+        }
+    };
+}
